bf213f441c183e7eecba1e1e3a682d6f0824abf7
Leonardo Brugnano <lebrug504@gmail.com>
2024-05-13 08:52:54 +0200

    Added check not to run as root

--- src/rls.c
+++ src/rls.c
@@ -8,6 +8,9 @@ int port;                    // server port number
 int
 main(int argc, char *argv[])
 {
+    if (getuid() == 0 || geteuid() == 0)
+        main_fail("ERROR: Running as root is extremely dangerous and hence not allowed.")
+
     if (!rls_init(argc, argv))
         main_fail("Failed to initialize.")
     
