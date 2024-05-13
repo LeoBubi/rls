af535e909a945ea5dc1f2140e89de9253d9275a3
Leonardo Brugnano <lebrug504@gmail.com>
2024-05-13 09:05:55 +0200

    isint() added check for longest integer

--- src/isint.c
+++ src/isint.c
@@ -7,9 +7,13 @@ isint(const char *s)
     if (s == NULL || *s == '\0')
         return 0;
 
-    for (int i = 0; s[i] != '\0'; i++)
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
         if (s[i] < '0' || s[i] > '9')
             return 0;
     
+    if (i > 10)     // 10 digits is the maximum for a 32-bit integer
+        return 0;
+
     return 1;
 }
