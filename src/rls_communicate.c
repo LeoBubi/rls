303ae3292d1d6dea9d56eb6cee9079198ae7c169
Leonardo Brugnano <lebrug504@gmail.com>
2024-05-15 14:23:42 +0200

    Finished rls_communicate() implementation

--- src/rls_communicate.c
+++ src/rls_communicate.c
@@ -11,6 +11,7 @@ rls_communicate(int sockfd)
 
     while (1)
     {
+        // wait for user input or server response
         fd_set readfds = __readfds;
         if (select(sockfd +1, &readfds, NULL, NULL, NULL) == -1) {
 #ifdef __DEBUG
@@ -21,9 +22,10 @@ rls_communicate(int sockfd)
 #endif
         }
 
-        // user input ready
+        // IF user input ready
         if (FD_ISSET(STDIN_FILENO, &readfds))
         {
+            // get user input
             char *input = userinput(NULL);
             if (!input) {
 #ifdef __DEBUG
@@ -34,10 +36,23 @@ rls_communicate(int sockfd)
                 return 0;
             }
 
+            // send user input size to server
+            size_t input_size = strlen(input) +1; // include null-terminator
+            if (write(sockfd, &input_size, sizeof(input_size)) == -1) {
+                free(input);
+#ifdef __DEBUG
+                perror("rls_communicate: send user input size: write");
+                return 0;
+#else
+                fun_fail("Communication error.")
+#endif
+            }
+
+            // send user input to server
             if (write(sockfd, input, strlen(input) +1) == -1) {
                 free(input);
 #ifdef __DEBUG
-                perror("rls_communicate: write");
+                perror("rls_communicate: send user input: write");
                 return 0;
 #else
                 fun_fail("Communication error.")
@@ -47,10 +62,50 @@ rls_communicate(int sockfd)
             free(input);
         }
 
-        // server response ready
+        // IF server response ready
         else if (FD_ISSET(sockfd, &readfds))
         {
-            // MAYBE YOU SHOULD INCLUDE A FIXED HEADER TO INDICATE THE LENGTH OF THE MESSAGE
+            // receive response size from server
+            size_t response_size;
+            ssize_t rb = read(sockfd, &response_size, sizeof(response_size));
+            if (rb == -1) {
+#ifdef __DEBUG
+                perror("rls_communicate: receive response size: read");
+                return 0;
+#else
+                fun_fail("Communication error.")
+#endif
+            }
+
+            // check if server closed connection
+            if (rb == 0) {
+                printf("Server closed connection.\n");
+                return 1;
+            }
+
+            // receive response from server and print it
+            char *response = (char*)malloc(response_size);
+            if (!response) {
+#ifdef __DEBUG
+                perror("rls_communicate: malloc");
+                return 0;
+#else
+                fun_fail("An unexpected error occurred.")
+#endif
+            }
+
+            if (read(sockfd, response, response_size) == -1) {
+                free(response);
+#ifdef __DEBUG
+                perror("rls_communicate: receive response: read");
+                return 0;
+#else
+                fun_fail("Communication error.")
+#endif
+            }
+
+            printf("%s\n", response);
+            free(response);
         }
     }
 }
