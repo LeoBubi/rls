7ce8dfac16ef657ff27c9ab0d7178d6fd4dc2ea0
Leonardo Brugnano <lebrug504@gmail.com>
2024-05-15 08:30:45 +0200

    finished rls_session() implementation

--- src/rls_session.c
+++ src/rls_session.c
@@ -9,6 +9,8 @@ extern struct in_addr server_ip;
 int
 rls_session(int sockfd)
 {
+    int ack;   // ACK value from server
+
     // send username to server
     if (write(sockfd, username, strlen(username) +1) == -1) {
 #ifdef __DEBUG
@@ -22,7 +24,6 @@ rls_session(int sockfd)
     // receive ACK from server
     // ACK = 1: user exists
     // ACK = 0: user doesn't exist or illegal (root)
-    int ack;
     if (read(sockfd, &ack, sizeof(ack)) == -1) {
 #ifdef __DEBUG
         perror("rls_session: receive username ack: read");
@@ -36,4 +37,58 @@ rls_session(int sockfd)
         printf("Illegal user.\n");
         return 0;
     }
+
+    else if (ack != 1) {
+#ifdef __DEBUG
+        fprintf(stderr, "rls_session: Illegal ACK value from server.\n");
+#endif
+        return 0;
+    }
+
+    // send password to server
+    char *password = userinput("Password: ");   // get password from user
+    if (!password) {
+#ifdef __DEBUG
+        fprintf(stderr, "rls_session: userinput failed.\n");
+#endif
+        return 0;
+    }
+
+    if (write(sockfd, password, strlen(password) +1) == -1) {
+        free(password);
+#ifdef __DEBUG
+        perror("rls_session: send password: write");
+        return 0;
+#else
+        fun_fail("Communication error.")
+#endif
+    }
+
+    free(password);
+
+    // receive ACK from server
+    // ACK = 1: password correct
+    // ACK = 0: password incorrect
+    if (read(sockfd, &ack, sizeof(ack)) == -1) {
+#ifdef __DEBUG
+        perror("rls_session: receive password ack: read");
+        return 0;
+#else
+        fun_fail("Communication error.")
+#endif
+    }
+
+    if (ack == 0) {
+        printf("Incorrect password.\n");
+        return 0;
+    }
+
+    else if (ack != 1) {
+#ifdef __DEBUG
+        fprintf(stderr, "rls_session: Illegal ACK value from server.\n");
+#endif
+        return 0;
+    }
+
+    return 1;  // user session established
 }
