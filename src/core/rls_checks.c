3a0484c7b39cefaa1db1abb41103d53d899615ba
Leonardo Brugnano <lebrug504@gmail.com>
2024-05-22 19:16:00 +0200

    minor

--- src/core/rls_checks.c
+++ src/core/rls_checks.c
@@ -7,6 +7,7 @@ rls_checks(void)
     // if root fail
     if (getuid() == 0 || geteuid() == 0)
         fun_fail("Running as root is unnecessary and hence not allowed.")
-    
+
+    // all checks passed
     return 1;
 }
